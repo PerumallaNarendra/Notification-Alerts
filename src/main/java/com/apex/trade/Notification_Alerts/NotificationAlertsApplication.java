157346cf1854c140be2538ae23e662829b2a21bc
Perumalla.Narendra <Perumalla.Narendra@apexon.com>
2025-09-18 16:58:06 +0530

    first commit

--- src/main/java/com/apex/trade/Notification_Alerts/NotificationAlertsApplication.java
+++ src/main/java/com/apex/trade/Notification_Alerts/NotificationAlertsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotificationAlertsApplication {
 
-    //Temporary commit
+    //first commit
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationAlertsApplication.class, args);
 	}
