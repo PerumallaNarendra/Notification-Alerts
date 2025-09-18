061ce50dac436df8ac400527600bebe04246a6ff
Perumalla Narendra Kumar <Perumalla.Narendra@apexon.com>
2025-09-18 16:50:02 +0530

    first commit

---
+++ src/main/java/com/apex/trade/Notification_Alerts/NotificationAlertsApplication.java
@@ -0,0 +1,14 @@
+package com.apex.trade.Notification_Alerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationAlertsApplication {
+
+    //Temporary commit
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationAlertsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apex/trade/Notification_Alerts/NotificationAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.trade.Notification_Alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
